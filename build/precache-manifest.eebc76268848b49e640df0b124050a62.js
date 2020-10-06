bc7c5678553558b3f4f4d8bd6cd4d3591d6b8856
Tai Tai <ywettai.dev@gmail.com>
2020-10-06 15:14:14 +0630

    add production files

---
+++ build/precache-manifest.eebc76268848b49e640df0b124050a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1220bf74057e6e3dc7a28f2941bd8f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c63719c1cef8a413da",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "2abad322d22211228ff6",
+    "url": "/static/js/2.949b0c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.949b0c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c63719c1cef8a413da",
+    "url": "/static/js/main.ab28a17b.chunk.js"
+  },
+  {
+    "revision": "f552f5337eb37e3f0768",
+    "url": "/static/js/runtime-main.5f2d4c4f.js"
+  }
+]);
