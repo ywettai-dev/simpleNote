f022c85df24ec0f9ebe028543f3c36ee711db128
Tai Tai <ywettai.dev@gmail.com>
2020-11-18 15:15:05 +0630

    refactor the project directory with modules

--- index.js
+++ index.js
@@ -1,77 +1,13 @@
 /* eslint-disable no-unused-vars */
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const Note = require('./models/note')
-const app = express()
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(express.json())
-app.use(express.static('build'))
-app.use(cors())
+const server = http.createServer(app)
 
-app.get('/api/notes', (req, res) => {
-    Note.find({}).then(notes => {
-        res.json(notes)
-    })
-})
-
-app.get('/api/notes/:id', (req, res, next) => {
-    Note.findById(req.params.id)
-        .then(note => {
-            if (note) {
-                res.json(note)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(err => next(err))
-})
-
-app.post('/api/notes', (req, res, next) => {
-    const body = req.body
-
-    if (body.content === undefined) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
-
-    note.save()
-        .then(savedNote => savedNote.toJSON())
-        .then(savedAndFormattedNote => {
-            res.json(savedAndFormattedNote)
-        })
-        .catch(err => next(err))
-
-})
-
-app.delete('/api/notes/:id', (req, res, next) => {
-    Note.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(err => next(err))
-})
-
-app.put('/api/notes/:id', (req, res, next) => {
-    const body = req.body
-
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(req.params.id, note, { new: true })
-        .then(updatedNote => {
-            res.json(updatedNote)
-        })
-        .catch(err => next(err))
+server.listen(config.PORT, () => {
+    logger.info(`Server running on PORT ${config.PORT}`)
 })
 
 const unknownEndpoint = (req, res) => {
@@ -93,8 +29,3 @@ const errorHandler = (error, req, res, next) => {
 }
 
 app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
-})
--- models/note.js
+++ models/note.js
@@ -1,18 +1,6 @@
 /* eslint-disable no-unused-vars */
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex:true })
-    .then(res => {
-        console.log('connected to MongoDB')
-    })
-    .catch(err => {
-        console.log('error connecting to MongoDB:', err.message)
-    })
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
