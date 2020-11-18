06e9c0a226d61ba6456a4105dd1b351be9eeaead
Tai Tai <ywettai.dev@gmail.com>
2020-11-18 19:46:02 +0630

    refactor index file in ref with app file

--- index.js
+++ index.js
@@ -9,23 +9,3 @@ const server = http.createServer(app)
 server.listen(config.PORT, () => {
     logger.info(`Server running on PORT ${config.PORT}`)
 })
-
-const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
-
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).send({ error: error.message })
-    }
-
-    next(error)
-}
-
-app.use(errorHandler)
